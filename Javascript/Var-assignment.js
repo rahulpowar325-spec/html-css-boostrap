3bce77a4320154b1d5d13ffe8073b889fb736bc9
Rahulpowar_11 <rahulpowar325@gmail.com>
2026-07-14 23:45:04 +0530

    add javascript var assignment

--- Javascript/Var-assignment.js
+++ Javascript/Var-assignment.js
@@ -1,1 +1,15 @@
+console.log("hello world....")
+let rahul1 = Symbol("unique");
+let rahul2 = Symbol("unique");
+console.log(symbol1 == symbol2);
 
+
+
+let favriateAnimal ={
+    name : "tiger",
+    type : "bangaliyan",
+    age : 20
+
+};
+console.log(favriateAnimal);
+console.log("your ang :" ,favriateAnimal.age);
