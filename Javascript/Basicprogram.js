245ad500cf9ab5dedc8bd3f612892fcf3727fb3a
Rahulpowar_11 <rahulpowar325@gmail.com>
2026-07-21 22:21:24 +0530

    learn function of js

--- Javascript/Basicprogram.js
+++ Javascript/Basicprogram.js
@@ -1,54 +1,63 @@
-// let x =5;
-// let y="6";
-// console.log(x+y);
+//Q1
+let x =5;
+let y="6";
+console.log(x+y);
 
-// let a;
-// console.log(typeof a);
+let a;
+console.log(typeof a);
  
-// console.log(10>5);
-// console.log(5 == "5");
-// console.log(5 ==="5");  // strict equal to num+type check
+console.log(10>5);
+console.log(5 == "5");
+console.log(5 ==="5");  // strict equal to num+type check
 
-// let num=5;
-// if(num>5){
-//     console.log("big");
+// Q2
+
+let num=5;
+if(num>5){
+    console.log("big");
     
-// }else {
-//     console.log("small");
+}else {
+    console.log("small");
     
-// }
+}
 
-// let char="hello" ,num=100 , bool=true , nudef= nudefine;
+//Q3
+let char="hello" ,num=100 , bool=true , nudef= nudefine;
 
-// console.log(typeof char);
-// console.log(typeof num);
-// console.log(typeof bool);
-// console.log(typeof nudef);
+console.log(typeof char);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof nudef);
 
-// let x="10";
-// let y=2;
-// console.log(x*y);
+//Q4
+let x="10";
+let y=2;
+console.log(x*y);
 
-// let value = "20"
-// value = Number(value)
-// console.log(typeof (value));
+let value = "20"
+value = Number(value)
+console.log(typeof (value));
 
-// let num =5;
-// let factorialValue=1;
 
-// for(i=1 ; i<=num; i++){
-//     factorialValue=factorialValue*i;
-//     console.log(i,factorialValue);
+//Q5
+let num =5;
+let factorialValue=1;
+
+for(i=1 ; i<=num; i++){
+    factorialValue=factorialValue*i;
+    console.log(i,factorialValue);
     
-// }
+}
 
-// for(i=1;i<=20;i++){
-//     if(i%2==0){
-//         console.log(i);
-//     }
+for(i=1;i<=20;i++){
+    if(i%2==0){
+        console.log(i);
+    }
     
     
-// }
+}
+
+//Q6
 
 let num = 13344;
 let resu = 0;
---
+++ Javascript/Funtion.js
@@ -0,0 +1,36 @@
+// function greet(){
+//  console.log("hello wold");
+
+     
+// }
+// greet()
+
+// function greetUser (name,age)
+// {
+//     console.log("hello", name ,"!", "youer age is",age);
+
+    
+// }
+// greetUser("piyush",20)
+
+// function add(a,b){
+//     return a+b
+// }
+// addition=add(10,20)
+// console.log(addition);
+
+// greet2=()=>console.log("hello")
+
+// addition1=(x,y)=>{
+//     add=x+y
+//     return add
+// }
+
+function multiplicationTable(num)
+{
+    for(i=1;i<=10;i++){
+        console.log(num,"*",i, "=", num*i);
+        
+    }
+}
+multiplicationTable(9)
