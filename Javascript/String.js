b9e4f34bedc6eec436189a663b13310a9ae6d5d1
Rahulpowar_11 <rahulpowar325@gmail.com>
2026-07-22 23:15:22 +0530

    today learn sting

---
+++ Javascript/String.js
@@ -0,0 +1,85 @@
+name1 ='rahul'
+name2="powar"
+name= name1+" "+name2
+console.log(name);
+
+console.log(name.length);  //str.length
+
+console.log(name.charAt(0)); //index 
+console.log(name[2]);
+
+console.log(name.toUpperCase());  //letter 
+console.log(name.toLowerCase());
+
+str1 ="hello world"
+str2="world"
+
+msg =str1.concat(" ",str2, "!!!")
+console.log(msg);
+
+
+indexNumber = name.indexOf("hellow")
+console.log(indexNumber);
+
+console.log(str1.indexOf('o'));
+console.log(str1.lastIndexOf('o'));
+
+
+//return boolean value true or false
+console.log(str1.includes('hello'));
+console.log(str1.includes('sdaddd'));
+
+//startWith()
+strr= "rahul powar hoe are you"
+console.log(strr.startsWith("are"))
+console.log(strr.endsWith("you"))
+
+//.slice() , .substring()  , .substr() 
+
+sub1=strr.slice(1,7)
+console.log(sub1);
+
+sub2=strr.substr(1,7)
+console.log(sub2);
+
+//.trim() , .trimStart(), .trimEnd() 
+
+strr1="        hellow      "
+console.log(strr1.trim().length);
+console.log(strr1.trimStart().length);  //starting space remove 
+console.log(strr1.trimEnd().length); //ending space remove
+
+
+//.replace()  and  .replaceAll().
+
+strr2= strr.replace('hoe' ,'hello')
+console.log(strr2);
+console.log(strr);
+
+
+
+strr3= "just do it, it dosen't mater "
+console.log(strr3.replaceAll("it","its" ));
+
+// .split)()
+fruits = 'apple bannana mango orenge'
+fruitsArrey = fruits.split()
+fruitsArrey = fruits.split(' ')
+
+console.log(fruitsArrey);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
