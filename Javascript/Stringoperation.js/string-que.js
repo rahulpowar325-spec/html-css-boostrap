d70b2be98cad28dfe2ab7135ddffbdd65d095ad5
Rahulpowar_11 <rahulpowar325@gmail.com>
2026-07-23 21:35:40 +0530

    changes

--- Javascript/Stringoperation.js/string-que.js
+++ Javascript/Stringoperation.js/string-que.js
@@ -1,97 +1,97 @@
-// // Q1 lower case conversion 
+// Q1 lower case conversion 
 
-// function convertToUpperCase(str){
-//     return str.toUpperCase()
+function convertToUpperCase(str){
+    return str.toUpperCase()
 
-// }
+}
 strpara="hello just do it"
-// str=convertToUpperCase(strpara)
-// console.log(str)
+str=convertToUpperCase(strpara)
+console.log(str)
 
 
-// // Q2lower case conversion----------------------------------------------------------------------------------------
+// Q2lower case conversion----------------------------------------------------------------------------------------
 
-// function convertToLowerCase(str){
-//     return str.toLowerCase()
-// }
-// lowerchar="HELLO HOE ARE YOU "
-// char = convertToLowerCase(lowerchar)
-// console.log(char);
+function convertToLowerCase(str){
+    return str.toLowerCase()
+}
+lowerchar="HELLO HOE ARE YOU "
+char = convertToLowerCase(lowerchar)
+console.log(char);
 
-// // Q3 extract the first world of sentance----------------------------------------------------------------------
+// Q3 extract the first world of sentance----------------------------------------------------------------------
 
-// const extractFristWord=(str) => {
-//     return str.charAt(0)
+const extractFristWord=(str) => {
+    return str.charAt(0)
 
-// }
-// firstLetter =extractFristWord(strpara)
-// console.log(firstLetter);
+}
+firstLetter =extractFristWord(strpara)
+console.log(firstLetter);
 
 
-// //Q4 split string into array ------------------------------------------------------------------------------------
+//Q4 split string into array ------------------------------------------------------------------------------------
 
-// const convertArrayWorld =(str)=>{
-//     return str.split(" ")
-// }
-// splitlatter=convertArrayWorld(strpara)
-// console.log(splitlatter);
+const convertArrayWorld =(str)=>{
+    return str.split(" ")
+}
+splitlatter=convertArrayWorld(strpara)
+console.log(splitlatter);
 
-// //Q5 reverse Array--------------------------------------------------------------------------------------------
+//Q5 reverse Array--------------------------------------------------------------------------------------------
 
-// // function SpaceWithHypen(){
-// //     arrayWolrd=str.split(" ")
-// //     SpaceWithHypen=arrayWolrd.join("-")
-// //     return SpaceWithHypen
-// // }
-// // strhypan=SpaceWithHypen(strpara)
-// // console.log(strhypan);
-
-// function SpaceWithHypen(str){
-//     SpaceWithHypen=str.split(" ").join("-")
+// function SpaceWithHypen(){
+//     arrayWolrd=str.split(" ")
+//     SpaceWithHypen=arrayWolrd.join("-")
 //     return SpaceWithHypen
 // }
 // strhypan=SpaceWithHypen(strpara)
 // console.log(strhypan);
 
-// //Q6 replace -------------------------------------------------------------------------------------------------
-// // function ReplaceSpace(str){
-// //     ReplaceSpace=str.replaceAll( "-" ," " )
-// //     return ReplaceSpace
-// // }
-// // replac=ReplaceSpace(strpara)
-// // console.log(replac);
+function SpaceWithHypen(str){
+    SpaceWithHypen=str.split(" ").join("-")
+    return SpaceWithHypen
+}
+strhypan=SpaceWithHypen(strpara)
+console.log(strhypan);
 
-// //short 
+//Q6 replace -------------------------------------------------------------------------------------------------
 // function ReplaceSpace(str){
-//     return str.replaceAll( "-" ," " )
+//     ReplaceSpace=str.replaceAll( "-" ," " )
+//     return ReplaceSpace
 // }
-// console.log(ReplaceSpace(strpara));
-// //--------------------------------------------------------------------------------------------------------------
-// name="rahul powar"
-// console.log(name.length-1);  //str.length
-// //-------------------------------------------------------------------------------------------------------------
+// replac=ReplaceSpace(strpara)
+// console.log(replac);
 
-// char="madakm"
-// function FristLastLater(str){
-//     first=str.charAt(0)
-//     lastlater=str.charAt(str.length-1)
-//     if(first==lastlater){
-//         return 'this string is palindrom'
+//short 
+function ReplaceSpace(str){
+    return str.replaceAll( "-" ," " )
+}
+console.log(ReplaceSpace(strpara));
+//--------------------------------------------------------------------------------------------------------------
+name="rahul powar"
+console.log(name.length-1);  //str.length
+//-------------------------------------------------------------------------------------------------------------
 
-//     }else{
-//         return 'this string is not palindrom'
-//     }
-// }
-// console.log(FristLastLater(char));
+char="madakm"
+function FristLastLater(str){
+    first=str.charAt(0)
+    lastlater=str.charAt(str.length-1)
+    if(first==lastlater){
+        return 'this string is palindrom'
+
+    }else{
+        return 'this string is not palindrom'
+    }
+}
+console.log(FristLastLater(char));
 
 
 // revers string -----------------------------------------------------------------------------------------------
 
 function reversstr(str){
     reverrsSTR=''
-        for(i=str.length-1; i>0 ;i--){
-            reverrsSTR= reverrsSTR+str[i]
-        }
-        return reverrsSTR
+    for(i=length-1; i>0 ;i--){
+          reverrsSTR= reverrsSTR=str[i]
+    }
+    return reverrsSTR
 }
 console.log(reversstr(strpara));
